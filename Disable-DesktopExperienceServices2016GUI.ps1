ac700dea6b628dfc067624ffef6894ae0c887cee
Aaron <aaroneg@users.noreply.github.com>
2018-03-28 15:36:50 -0500

    Update Disable-DesktopExperienceServices2016GUI.ps1

--- Disable-DesktopExperienceServices2016GUI.ps1
+++ Disable-DesktopExperienceServices2016GUI.ps1
@@ -1,1 +1,41 @@
+# Disable the stupid, useless services that 2016 in GUI mode comes with by default to reduce attack surface
+# Refer to https://docs.microsoft.com/en-us/windows-server/security/windows-services/security-guidelines-for-disabling-system-services-in-windows-server
+$DisableServices=@(
+    'XblGameSave',
+    'XblAuthManager',
+    'MapsBroker',
+    'AxInstSV',
+    'AJRouter',
+    'bthserv',
+    'dmwappushservice',
+    'lfsvc',
+    'SharedAccess',
+    'wlidsvc',
+    'NcbService',
+    'PhoneSvc',
+    'PcaSvc',
+    'QWAVE',
+    'RmSvc',
+    'SensorDataService',
+    'SensrSvc',
+    'SensorService',
+    'ShellHWDetection',
+    'ScDeviceEnum',
+    'SSDPSRV',
+    'WiaRpc',
+    'upnphost',
+    'WalletService',
+    'Audiosrv',
+    'AudioEndpointBuilder',
+    'FrameServer',
+    'stisvc',
+    'wisvc',
+    'WpnService'
+)
+
+ForEach-Object -InputObject $DisableServices {Get-Service -Name $_ | Set-Service -StartupType Disabled}
+
+# Disable scheduled tasks for XBox
+Get-ScheduledTask -TaskName XblGameSaveTask | Disable-ScheduledTask
+Get-ScheduledTask -TaskName XblGameSaveTaskLogon | Disable-ScheduledTask
 
