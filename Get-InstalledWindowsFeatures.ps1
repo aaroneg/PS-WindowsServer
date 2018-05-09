17b23431c216ef1da5b1266fc005a7c122f4a6e2
Aaron <aaroneg@users.noreply.github.com>
2018-05-09 15:49:35 -0500

    Update Get-InstalledWindowsFeatures.ps1

--- Get-InstalledWindowsFeatures.ps1
+++ Get-InstalledWindowsFeatures.ps1
@@ -1,2 +1,4 @@
+# Find all installed features
 $InstalledFeatures=Get-WindowsFeature |Where-Object {$_.Installed -eq $true}
+# Save them to disk
 $InstalledFeatures|Export-Clixml "$PSScriptRoot\$($env:COMPUTERNAME)Features.xml"
